bcc2792537fc49d352c7070c9137a182f8b511df
Rahul <rahulshukla03@outlook.com>
2020-05-14 08:45:37 +0530

    first time

---
+++ PromotionEngine/PromotionEngine/Cashier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PromotionEngine
+{
+    public class Cashier
+    {
+        private List<IPricingStrategy> _pricingStrategies;
+
+        public Cashier(List<IPricingStrategy> pricingStrategies)
+        {
+            _pricingStrategies = pricingStrategies;
+        }
+
+        public double Checkout(IList<Sku> products)
+        {
+            double result = 0;
+            foreach (var strat in _pricingStrategies)
+            {
+                var prods = products.Where(p => p == strat.Sku);
+                result = result + strat.GetPrice(prods.Count());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PromotionEngine/PromotionEngine/SaleStrategies.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public abstract class SaleStrategy : IPricingStrategy
+    {
+        public abstract Sku Sku { get; }
+        protected abstract double PricePerOne { get; }
+        protected abstract double PricePerX { get; }
+        protected abstract int X { get; }
+
+        public double GetPrice(int count)
+        {
+            if (count == 0)
+            {
+                return 0;
+            }
+
+            double result = 0;
+
+            while (count >= X)
+            {
+                result = result + PricePerX;
+                count = count - X;
+            }
+
+            return result + (PricePerOne * count);
+        }
+    }
+
+    public class PricingStategyA : SaleStrategy
+    {
+        public override Sku Sku { get; } = 'A';
+        protected override double PricePerOne { get; } = 50;
+        protected override double PricePerX { get; } = 130;
+        protected override int X { get; } = 3;
+    }
+
+    public class PricingStrategyB : SaleStrategy
+    {
+        public override Sku Sku { get; } = 'B';
+        protected override double PricePerOne { get; } = 30;
+        protected override double PricePerX { get; } = 45;
+        protected override int X { get; } = 2;
+    }
+}
